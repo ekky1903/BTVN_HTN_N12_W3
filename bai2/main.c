aa542154a9ebae2af232ee86ff8c67a44fca6871
ekky1903 <dangtienod2005@gmail.com>
2026-09-25 13:44:04 +0700

    Update main.c

--- bai2/main.c
+++ bai2/main.c
@@ -1,16 +1,16 @@
 #include "stm32f10x.h"
 #include "max7219.h"
 
-// Mảng dữ liệu: Hình tam giác cân (hướng lên trên)
+
 const uint8_t triangle[8] = {
-    0x00, // 00000000 (Hàng 1: Trống)
+    0x00, // 00000000 
     0x18, // 00011000 (Hàng 2: Đỉnh tam giác rộng 2 chấm)
     0x3C, // 00111100 (Hàng 3)
     0x7E, // 01111110 (Hàng 4)
     0xFF, // 11111111 (Hàng 5: Đáy tam giác)
     0x00, // 00000000 (Hàng 6: Trống)
-    0x00, // 00000000 (Hàng 7: Trống)
-    0x00  // 00000000 (Hàng 8: Trống)
+    0x00, // 00000000 
+    0x00  // 00000000 
 };
 
 int main(void) {
