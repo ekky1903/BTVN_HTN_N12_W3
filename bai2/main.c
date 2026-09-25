50d50bb795c04ad825f8b7424b68f0a18da2c05a
ekky1903 <dangtienod2005@gmail.com>
2026-09-25 13:43:14 +0700

    Update main.c

--- bai2/main.c
+++ bai2/main.c
@@ -1,29 +1,29 @@
 #include "stm32f10x.h"
 #include "max7219.h"
 
-// Mảng dữ liệu: Hình trái tim tĩnh
-const uint8_t heart_rotated[8] = {
-    0x30, // 00110000
-    0x78, // 01111000
-    0x7C, // 01111100
-    0x3E, // 00111110
-    0x3E, // 00111110
-    0x7C, // 01111100
-    0x78, // 01111000
-    0x30  // 00110000
+// Mảng dữ liệu: Hình tam giác cân (hướng lên trên)
+const uint8_t triangle[8] = {
+    0x00, // 00000000 (Hàng 1: Trống)
+    0x18, // 00011000 (Hàng 2: Đỉnh tam giác rộng 2 chấm)
+    0x3C, // 00111100 (Hàng 3)
+    0x7E, // 01111110 (Hàng 4)
+    0xFF, // 11111111 (Hàng 5: Đáy tam giác)
+    0x00, // 00000000 (Hàng 6: Trống)
+    0x00, // 00000000 (Hàng 7: Trống)
+    0x00  // 00000000 (Hàng 8: Trống)
 };
 
 int main(void) {
     SystemInit();
     
     // Delay khởi động chờ ổn định nguồn
-    for(volatile int i = 0; i < 500000; i++);
+    for (volatile int i = 0; i < 500000; i++);
 
     MAX7219_Init();
 
-    // Đẩy 8 byte của hình trái tim đã xoay ra ma trận LED
+    // Xuất 8 byte dữ liệu ra 8 dòng của ma trận LED
     for (int i = 0; i < 8; i++) {
-        MAX7219_Write(i + 1, heart_rotated[i]);
+        MAX7219_Write(i + 1, triangle[i]);
     }
 
     while (1) {
